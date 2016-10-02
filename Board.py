89e2e170faef45f6ee9d6c029a05b62057918288
Nicholas Maltbie <nick.dmalt@gmail.com>
2016-10-02 19:01:50 -0400

    Started work on board. Componenets still need testing

--- Board.py
+++ Board.py
@@ -7,8 +7,30 @@ pieces and stables), a market street (or streets), towers with walls branching
 off them, and a well.
 """
 
+import random
 from Player import *
 from Location import *
 from Building import *
 from Market import *
 from Tower import *
+
+def make_board(rows, columns):
+    """Makes a board with a default game setup, 
+    One well will be randomly placed.
+    A set of towers will be made.
+    A market will be created with a randomly placed merchant.
+    
+    A board has Buildings, a market, towers, and a well
+    """
+    well_location = make_location(random.randrange(rows - 2) + 1, \
+        random.randrange(columns - 2) + 1)
+    market_start = make_location(random.randrange(rows - 2) + 1, \
+        random.randrange(columns - 2) + 1)
+    while market_start == well_location:
+        market_start = make_location(random.randrange(rows - 2) + 1, \
+            random.randrange(columns - 2) + 1)
+        
+    return {'Rows':rows, 'Columns':columns, 'Buildings':[], \
+        'Market':make_market(street_start), 'Towers':make_towers(rows, columns), 
+        'Well':well_location}
+
