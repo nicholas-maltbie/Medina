f74b94583e016989fdd7a1b74f7a263cb447fcc1
Jeet Shah <Jeet@jeets-mbp.rhod.uc.edu>
2016-11-07 22:47:30 -0500

    Added doctests to Move,JSON Formating, Finished file

--- Move.py
+++ Move.py
@@ -5,9 +5,31 @@ of different pieces of information:
 player_name: name
 move_type: {PASS or NONE_POSSIBLE or NORMAL}
 piece_played: {WALL or BUILDING or STABLE or MERCHANT or ROOFTOP or NO_PLAY}
-location: {row, column} """
+location: {row, column} 
+
+>>> move = make_move('Bob',PASS)
+>>> get_player_name(move)
+'Bob'
+>>> get_move_type(move)
+'PASS'
+>>> get_piece(move) is None
+True
+>>> get_location(move) is None
+True
+
+>>> move = make_move('Bob',NORMAL,WALL,(1,2))
+>>> get_player_name(move)
+'Bob'
+>>> get_move_type(move)
+'NORMAL'
+>>> get_piece(move)
+'WALL'
+>>> get_location(move)
+[1, 2]
+"""
 
 import Location
+import json
 
 #Move types
 PASS = 'PASS'
@@ -23,23 +45,34 @@ ROOFTOP = 'ROOFTOP'
 NO_PLAY = 'NO PLAY'
 
 
-def make_move(player_name, move_type, peice_type=None, location=None):
-    return ""
+def make_move(player_name, move_type, piece_type=None, location=None):
+    if move_type != NORMAL:
+        assert piece_type == None and location == None
+
+    return json.dumps({"player_name": player_name, "move_type": move_type, "piece_type": piece_type, "location": location})
     #this should return a move with the following information in a JSON format
 
 def get_player_name(move):
-    pass
+    getter = json.loads(move)
+    return getter['player_name']
     #This should parse a move and return the player name
 
 def get_move_type(move):
-    pass
+    getter = json.loads(move)
+    return getter['move_type']
     #This should parse the move and reutrn either 'PASS', 'NONE_POSSIBLE', or 'NORMAL'
 
-def get_peice(move):
-    pass
+def get_piece(move):
+    getter = json.loads(move)
+    if get_move_type(move) != NORMAL:
+        return None
+    return getter['piece_type']
     #This should parse the move and return the piece type played (or NO_PLAY)
     
 def get_location(move):
-    pass
+    getter = json.loads(move)
+    if get_move_type(move) != NORMAL:
+        return None
+    return getter['location']
     #This should return a location of where the move specifies
 
