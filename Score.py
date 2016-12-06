861e5b1fcea6b69211a2e37dca7ade52ec095ef1
cygnet <12aaron93@gmail.com>
2016-12-05 20:56:24 -0500

    Changed well graphic

---
+++ Score.py
@@ -0,0 +1,10 @@
+"""This module will determine player's score in a game given the board and 
+player"""
+
+from Board import *
+from Player import *
+from Tile import *
+from Building import *
+
+def get_score(board, player)
+    
